a531c5ac6657c24c73ef382e5264cb0df4f3b7f0
Frank Werner <mail@hb9fxq.ch>
2020-11-20 15:01:46 +0100

    - refactorings - added slices object - mqtt command

--- assets/build.sh
+++ assets/build.sh
@@ -8,13 +8,18 @@ cd ..
 # Linux
 env GOOS=linux GOARCH=amd64 go build -o ../../../../bin/flexlib-go/linux64/iq-transfer github.com/hb9fxq/flexlib-go/cmd/iq-transfer
 env GOOS=linux GOARCH=386 go build -o ../../../../bin/flexlib-go/linux32/iq-transfer github.com/hb9fxq/flexlib-go/cmd/iq-transfer
+env GOOS=linux GOARCH=amd64 go build -o ../../../../bin/flexlib-go/linux64/MqttAdapter github.com/hb9fxq/flexlib-go/cmd/MqttAdapter
+env GOOS=linux GOARCH=386 go build -o ../../../../bin/flexlib-go/linux32/MqttAdapter github.com/hb9fxq/flexlib-go/cmd/MqttAdapter
 
 # Raspi
 env GOOS=linux GOARCH=arm GOARM=5 go build -o ../../../../bin/flexlib-go/raspberryPi/iq-transfer github.com/hb9fxq/flexlib-go/cmd/iq-transfer
+env GOOS=linux GOARCH=arm GOARM=5 go build -o ../../../../bin/flexlib-go/raspberryPi/MqttAdapter github.com/hb9fxq/flexlib-go/cmd/MqttAdapter
 
 # Windows
-env GOOS=windows GOARCH=amd64 go build -o ../../../../bin/flexlib-go/Win64/iq-transfer.exe github.com/hb9fxq/flexlib-go/cmd/iq-transfer
-env GOOS=windows GOARCH=386 go build -o ../../../../bin/flexlib-go/Win32/iq-transfer.exe github.com/hb9fxq/flexlib-go/cmd/iq-transfer
+env GOOS=windows GOARCH=amd64 go build -o ../../../../bin/flexlib-go/Win64/MqttAdapter.exe github.com/hb9fxq/flexlib-go/cmd/iq-transfer
+env GOOS=windows GOARCH=386 go build -o ../../../../bin/flexlib-go/Win32/MqttAdapter.exe github.com/hb9fxq/flexlib-go/cmd/iq-transfer
+env GOOS=windows GOARCH=amd64 go build -o ../../../../bin/flexlib-go/Win64/MqttAdapter.exe github.com/hb9fxq/flexlib-go/cmd/MqttAdapter
+env GOOS=windows GOARCH=386 go build -o ../../../../bin/flexlib-go/Win32/MqttAdapter.exe github.com/hb9fxq/flexlib-go/cmd/MqttAdapter
 
 
 # pfsense
